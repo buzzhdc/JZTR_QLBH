9dc1ae3c40d99426c661353f89495e49007afbcf
thanh <thanh@ThanhBeen>
2021-07-28 08:50:22 +0700

    Hí ae, mình mới viết chức năng update vs thêm sp, ae kiểm thử coi sao nhé

--- src/model/SanPhamDAO.java
+++ src/model/SanPhamDAO.java
@@ -7,21 +7,23 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ductr
  */
 public class SanPhamDAO implements SanPhamInterface{
-    Connection cn;
+    Connection connec;
     String url;
     public SanPhamDAO(){
-        url="jdbc:sqlserver://localhost:1433;databaseName=QLBANHANG;userName=sa;password=123";
+        url="jdbc:sqlserver://localhost:1433;databaseName=QLBANHANG;userName=QLBanHangAdmin;password=12345";
         try{
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            cn=DriverManager.getConnection(url);
+            connec=DriverManager.getConnection(url);
         }catch(ClassNotFoundException e){
             e.printStackTrace();
         }catch(SQLException e){
@@ -30,14 +32,61 @@ public class SanPhamDAO implements SanPhamInterface{
     }
     @Override
     public int themSanPham(SanPham sp) {
-        
-        int kq=0;
-        return kq;
+        try {
+            int kq = 0;
+            PreparedStatement ps = connec.prepareStatement(
+                    "insert into HangHoa values( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            ps.setInt(1, sp.getLoHang());
+            ps.setInt(2, sp.getMaLoai());
+            ps.setString(3, sp.getTenHang());
+            ps.setDouble(4, sp.getGiaNhap());
+            ps.setDouble(5, sp.getGiaBan());
+            ps.setInt(6, sp.getSoLuong());
+            ps.setInt(7, sp.getSoLuongMin());
+            ps.setString(8, sp.getNSX());
+            ps.setString(9, sp.getHSD());
+            ps.setInt(10, sp.getHSDMin());
+            kq = ps.executeUpdate();//Trả về 0 đăng nhập thất bại, !=0 thành công
+            ps.clearBatch();
+            ps.close();
+            connec.close();
+            //connec.rollback();
+            return kq;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Lỗi");
+            e.printStackTrace();
+        }
+        return 0;
     }
 
-    @Override
-    public int updateSanPham() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //@Override
+    public int updateSanPham(SanPham sp) {
+        int kq = 0;
+        try {
+            PreparedStatement ps = connec.prepareStatement(
+                    "update HangHoa set LOHANG = ?, MALOAI = ?, TENHANG =?, GIANHAP =?, "
+                            + "GIABAN = ?, SOLUONG =?, SOLUONGMIN =?, NSX =?, HSD =?, HSDMIN =? WHERE MAHANG = ?");
+            ps.setInt(1, sp.getLoHang());
+            ps.setInt(2, sp.getMaLoai());
+            ps.setString(3, sp.getTenHang());
+            ps.setDouble(4, sp.getGiaNhap());
+            ps.setDouble(5, sp.getGiaBan());
+            ps.setInt(6, sp.getSoLuong());
+            ps.setInt(7, sp.getSoLuongMin());
+            ps.setString(8, sp.getNSX());
+            ps.setString(9, sp.getHSD());
+            ps.setInt(10, sp.getHSDMin());
+            ps.setInt(11, sp.getMaHang());
+            kq = ps.executeUpdate();
+            ps.close();
+            connec.close();
+            return kq;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Lỗi");
+            ex.printStackTrace();
+        }
+        //connec.setAutoCommit(true);
+        return 0;
     }
 
     @Override
@@ -52,6 +101,11 @@ public class SanPhamDAO implements SanPhamInterface{
 
     @Override
     public ArrayList search() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateSanPham() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
