dc83f1342772b27dde9e010125eb4fb7d5a0cf01
thanh <thanh@ThanhBeen>
2021-08-04 19:12:51 +0700

    Hí ae

---
+++ src/model/KhachHangDAO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author thanh
+ */
+public class KhachHangDAO {
+    Connection cn;
+    String url;
+
+    public KhachHangDAO() {
+        url="jdbc:sqlserver://localhost:1433;databaseName=QLBANHANG;userName=QLBanHangAdmin;password=12345";
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            cn=DriverManager.getConnection(url);
+            System.out.println("Connect thành công");
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public int ThemKhachHang(KhachHang kh) {
+        int kq=0;
+        try{
+            PreparedStatement ps= cn.prepareStatement
+        ("insert into KhachHang values(?,?,?,?,?)");
+            ps.setString(1, kh.getTenKH());
+            ps.setString(2, kh.getdChi());
+            ps.setBoolean(3,kh.isgTinh());
+            ps.setString(4, kh.getNgSinh());
+            ps.setString(5, kh.getPhone());
+            kq=ps.executeUpdate();
+            ps.clearBatch();
+            ps.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return kq;
+    }
+}
